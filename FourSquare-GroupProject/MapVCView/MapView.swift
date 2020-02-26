0b52c5ae8d675e7a5b987a7c69b29e71c85165a9
uwagboe <sm.uwagboe@gmail.com>
2020-02-26 15:46:53 -0500

    the app now updates the location and the items around based on what the user types git push
    :q

--- FourSquare-GroupProject/MapVCView/MapView.swift
+++ FourSquare-GroupProject/MapVCView/MapView.swift
@@ -11,12 +11,21 @@ import MapKit
 
 class MapView: UIView {
     
+    private lazy var panGesture: UIGestureRecognizer = {
+        let gesture = UITapGestureRecognizer()
+                 gesture.addTarget(self, action: #selector(didPanGesture(_:)))
+        
+                 return gesture
+        
+    }()
+    
     public lazy var searchTheArea: UITextField = {
        let areaTextField = UITextField()
         areaTextField.autocapitalizationType = .none
         areaTextField.placeholder = "tell us the AREA you are interested in"
         areaTextField.backgroundColor = .white
         areaTextField.borderStyle = .roundedRect
+        
         return areaTextField
     }()
     
@@ -29,7 +38,7 @@ class MapView: UIView {
            return foodTextField
        }()
     
-    public lazy var avenueMapView: MKMapView =    {
+    public lazy var actualMapView: MKMapView =    {
        let mapView = MKMapView()
         return mapView
     }()
@@ -56,19 +65,47 @@ class MapView: UIView {
        setupAvenueMapView()
         setUpAreaSearchBar()
         setUpFoodSearchBar()
+        // registers what is being done... because the interacton of the gesture should be recognized when the map is moved so it can populate properly.
+        actualMapView.isUserInteractionEnabled = true
+        addGestureRecognizer(panGesture)
     }
     
-    private func setupAvenueMapView()   {
-        addSubview(avenueMapView)
+    @objc func didPanGesture(_ gesture: UILongPressGestureRecognizer) {
+        print("pan gesture is RECOGNIZED")
         
-        avenueMapView.translatesAutoresizingMaskIntoConstraints = false
+        // want the annotations to appear based on whatever region it displayed
+        if gesture.state == .began || gesture.state == .changed {
+            print("we panning bitch")
+            return
+        }
+        
+    
+        
+        reloadTheAnnotations()
+    }
+    
+    
+    private func reloadTheAnnotations() {
+         let controller = MapController()
+        
+        // inside of loadMap the annotation func that has a return value gets called and used.
+        
+        controller.loadMap()
+        
+    }
+    
+    
+    private func setupAvenueMapView()   {
+        addSubview(actualMapView)
+        
+        actualMapView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
         
-            avenueMapView.topAnchor.constraint(equalTo: topAnchor),
-            avenueMapView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            avenueMapView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            avenueMapView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
+            actualMapView.topAnchor.constraint(equalTo: topAnchor),
+            actualMapView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            actualMapView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            actualMapView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
         
         ])
     }
@@ -82,7 +119,7 @@ class MapView: UIView {
                 searchTheArea.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
                    searchTheArea.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
                    searchTheArea.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
-                   searchTheArea.heightAnchor.constraint(equalToConstant: 40)
+                 //  searchTheArea.heightAnchor.constraint(equalToConstant: 40)
                 //   searchTheArea.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)
                ])
         
@@ -98,7 +135,7 @@ class MapView: UIView {
                         searchTheKindOfFood.topAnchor.constraint(equalTo: searchTheArea.bottomAnchor),
                           searchTheKindOfFood.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
                           searchTheKindOfFood.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
-                          searchTheArea.heightAnchor.constraint(equalToConstant: 25)
+                         // searchTheArea.heightAnchor.constraint(equalToConstant: 25)
                       
                       ])
     }
