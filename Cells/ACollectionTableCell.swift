4f0e6531755aa5a5b1da3d319bf91a2d0f71a2ed
Eric Davenport <EricDavenport@pursuit.org>
2020-02-27 14:51:04 -0500

    loading temporary photo in aCollectionCell

--- Cells/ACollectionTableCell.swift
+++ Cells/ACollectionTableCell.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class ACollectionTableCell: UITableViewCell {
-
+  
   public lazy var venueImage : UIImageView = {
     let iv = UIImageView()
-    iv.backgroundColor = .white
+//    iv.backgroundColor = .white
     return iv
   }()
   
@@ -36,8 +36,8 @@ class ACollectionTableCell: UITableViewCell {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     commonInit()
   }
-
-
+  
+  
   
   required init?(coder: NSCoder) {
     super.init(coder: coder)
@@ -49,7 +49,7 @@ class ACollectionTableCell: UITableViewCell {
     titleLabelConstraint()
     addressLabelConstraint()
   }
-
+  
   private func imageConstraints() {
     addSubview(venueImage)
     venueImage.translatesAutoresizingMaskIntoConstraints = false
@@ -66,7 +66,7 @@ class ACollectionTableCell: UITableViewCell {
     titleLabel.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint.activate([
       titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10),
-      titleLabel.leadingAnchor.constraint(equalTo: venueImage.trailingAnchor, constant: 10),
+      titleLabel.leadingAnchor.constraint(equalTo: venueImage.trailingAnchor, constant: 20),
       titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10)
     ])
   }
@@ -76,9 +76,52 @@ class ACollectionTableCell: UITableViewCell {
     addressLabel.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint.activate([
       addressLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
-      addressLabel.leadingAnchor.constraint(equalTo: venueImage.trailingAnchor, constant: 10),
+      addressLabel.leadingAnchor.constraint(equalTo: venueImage.trailingAnchor, constant: 20),
       addressLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10)
     ])
+  }
+  
+  public func configCell() {
+
+    PhotoAPIClient.photoURL(venueID: "") { [weak self] (result) in
+      switch result {
+      case .failure(let appError1):
+        print("fail: \(appError1)")
+      case .success(let url):
+        print("success - loading image")
+        DispatchQueue.main.async {
+          self?.imageView?.getImage(with: url, completion: { (result) in
+            switch result {
+            case .failure(let appError):
+              print("Error: \(appError)")
+            case .success(let image):
+              self?.titleLabel.text = "Skunk"
+              self?.addressLabel.text = "Trash"
+              print("complete")
+              DispatchQueue.main.async {
+                self?.imageView?.image = image
+                
+              }
+            }
+          })
+        }
+      }
+    }
+  }
+  
+  public func loadPhoto() {
+    let url = "https://fastly.4sqi.net/img/general/original/82007_cHf3A0muNVwivg_w1lfjVMkU7K99xN1txQwb2TjWC6E.jpg"
+    imageView?.getImage(with: url, completion: { (result) in
+      switch result {
+      case .failure(let appError):
+        print("AppError: \(appError)")
+      case .success(let image):
+        DispatchQueue.main.async {
+          print("loaded")
+          self.imageView?.image = image
+        }
+      }
+    })
   }
   
 }
